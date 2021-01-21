a465a9a570847c31262b916f5e8e50099cee4ce3
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-01-21 13:32:51 -0300

    Manipulando arquivos - READ

--- Modulo3/tests/01/service.js
+++ Modulo3/tests/01/service.js
@@ -5,7 +5,7 @@ const URL = `https://swapi.dev/api/people`
 async function obterPessoas(nome) {
     const url = `${URL}/?search=${nome}&format=json`
     const result = await get(url)
-    console.log(JSON.stringify(result.data))
+    // console.log(JSON.stringify(result.data))
     return result.data.results.map(mapearPessoas)
 }
 
