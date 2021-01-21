cf52f7f3ac0f222a42471c5a9fbc251522c768fa
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-01-21 11:09:00 -0300

    Criando Array.Reduce

---
+++ Modulo2/lists/for-forin/4.reduce.js
@@ -0,0 +1,35 @@
+const {obterPessoas} = require('./service')
+
+Array.prototype.meuReduce = function(callback, valorInicial){
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0]
+    for(let index = 0; index <= this.length -1; index ++){
+        valorFinal = callback(valorFinal, this[index], this)
+    }
+    return valorFinal
+}
+
+async function main() {
+    try{
+
+        const { results } = await obterPessoas(`a`)
+        const pesos = results.map(item => parseInt(item.height))
+        console.log("Pesos: ",pesos)
+        // [20.2, 30.5, 40.5] = 0
+        // const total = pesos.reduce((anterior, proximo) => {
+        //     return anterior + proximo
+        // }, 0)
+        const minhaLista = [
+            ['Paulo', 'Sarmento'],
+            ['NodeBR', 'Nerdzão']
+        ]
+        const total = minhaLista.meuReduce((anterior, proximo) => {
+            return anterior.concat(proximo)
+        }, []).join(', ')
+        console.log('Total: ', total)
+    }
+    catch(error){
+        console.log("DEU RUIM", error)
+    }
+
+}
+main()
