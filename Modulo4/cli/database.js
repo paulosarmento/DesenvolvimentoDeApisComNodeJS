da6282d36d5d89a48994627c57d99b3f346762b2
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-01-22 10:38:51 -0300

    Manipulando arquivos - DELETE

--- Modulo4/cli/database.js
+++ Modulo4/cli/database.js
@@ -2,6 +2,7 @@ const {readFile, writeFile} = require('fs')
 
 const {promisify} = require('util')
 
+// para não precisar mais trabalhar com callback transforma em promise usamos o async await para manipular as coisas
 const readFileAsync = promisify(readFile)
 const writeFileAsync = promisify(writeFile)
 
@@ -9,21 +10,28 @@ const writeFileAsync = promisify(writeFile)
 // const dadosJson = require('./herois.json')
 
 class Database{
+    //objetos
     constructor(){
         this.NOME_ARQUIVO = 'herois.json'
     }
 
     async obterDadosArquivo(){
+        // obter toddos os dados no formato de utf8
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8') 
+        // retorna o arquivo como JSON fazendo o parse do objeto
         return JSON.parse(arquivo.toString())       
     }
     async escreverArquivo(dados){
+        // escrever dados no banco de dados 
         await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        // caso der certo retorna verdadeiro
         return true
     }
 
     async cadastrar(heroi) {
+        // obtem os dados como uma lista
         const dados = await this.obterDadosArquivo()
+        // cria um id e se heroi for menos que 2 se não existir cria um heroi baseado na hora -> simular que a gente esta criando um id 
         const id = heroi.id <= 2 ? heroi.id : Date.now()
         /*
         {
@@ -64,16 +72,41 @@ class Database{
     //            nome: Batman
     //        }
     //    ]
+        // cadastra o heroi
         const resultado = await this.escreverArquivo(dadosFinal)
-
+        // retorna o resultado
         return resultado
     }
 
+
     async listar(id){
+        // obter o arquivo inteiro
         const dados = await this.obterDadosArquivo()
+        // faz um filtro nos item buscando o id que a gente passar se não passar id manda a lista inteira 
         const dadosFiltrados = dados.filter(item => (id ? (item.id  === id) : true))
+        // retorna o dado
         return dadosFiltrados
+    }
+    async remover(id) {
+        // se nao mandou nenhum id remove todos os itens da base de dados 
+        if(!id){
+            return await this.escreverArquivo([])
+        }
+        // obter os dados do banco de dedos 
+        const dados = await this.obterDadosArquivo()
+        // procurar um item dentro da lista JSON. findIndex  -> consigo passar uma funcao para retornar um boolean se existir 
+        const indice = dados.findIndex(item=> item.id === parseInt(id))
+        // se não tiver nenhum item no JSON retorna erro
+        if(indice === -1){
+            throw Error('O usuario informado não existe')
+        }
+        // pegar o indice da lista e remove 1 unico item 
+        dados.splice(indice, 1)
+
+        // registra de novo na base de dados
+        return await this.escreverArquivo(dados)
     }
 }
 
+// exporta a instancia para não precisar ficar instanciando quando precisar usar para obter os objetos
 module.exports = new Database()
--- Modulo4/cli/test.js
+++ Modulo4/cli/test.js
@@ -1,5 +1,6 @@
 const {deepEqual, ok} = require ('assert')
 
+// Objeto moledo 
 const DEFAULT_ITEM_CADASTRAR = {
     nome: 'Flash',
     poder: 'Speed',
@@ -8,23 +9,36 @@ const DEFAULT_ITEM_CADASTRAR = {
 const database = require('./database')
 
 describe('Suite de manipulação de Herois', () => {
-
-    // before(async () => {
-    //     await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
-    // })
+    // antes de todo processo cadastra um heroi
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
 
     it('deve pesquisar um heroi usando arquivos', async () => {
+        // Objeto esperado
         const expected = DEFAULT_ITEM_CADASTRAR
+        // pega o primeiro objeto do array pelo id do objeto esperado
         const [resultado] = await database.listar(expected.id)
-
+        // faz o test 
         deepEqual(resultado, expected)
     })
 
     it('deve cadastrar um heroi, usando arquivos', async () => {
+        // Objeto esperado
         const expected = DEFAULT_ITEM_CADASTRAR
+        // Cadastrar heroi na base de dados
         const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        // procura o heroi cadastrado e procura ele pelo id e verifica se ele foi cadastrado pegando o primeiro objeto do array
         const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
-
+        // faz o test
         deepEqual(actual, expected)
+    })
+    it('deve remover um heroi por id', async () => {
+        //caso der certo retorna verdadeiro
+        const expected = true
+        // remover o heroi
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(resultado, expected)
+
     })
 })
